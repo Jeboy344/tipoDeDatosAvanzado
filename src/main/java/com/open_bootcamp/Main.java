9d076c4fee60197cc0fef81cdedb67a9e2499a28
Jeboy <tovilajesus@gmail.com>
2023-05-22 20:46:51 -0300

    String

---
+++ src/main/java/com/open_bootcamp/Main.java
@@ -0,0 +1,31 @@
+package com.open_bootcamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*String cadena = "Mensaje DE TEXTo";
+
+        int cadenaLen = cadena.length();
+        System.out.println("La longitud de mi cadena es de: " + cadenaLen);
+
+        String cadenaMinus = cadena.toLowerCase();
+        System.out.println("La cadena ahora es: " + cadenaMinus);
+
+        String cadenaMayus = cadena.toUpperCase();
+        System.out.println("La cadena ahora es: " + cadenaMayus);
+
+        boolean resultado = cadena.startsWith("Men"); // .endsWith("") (verifica que la cadena acabe por el valor que pasemos por parametro)
+        if (resultado) {
+            System.out.println("Empieza por lo que estoy buscando");
+        } else {
+            System.out.println("No empieza por lo que quiero");
+        }
+
+        char letra = cadena.charAt(0); // Me indica el caracter según el valor que la posición que le pase por parametro.
+        System.out.println("Caracter es: " + letra);
+
+        for (int i = 0; i < cadena.length(); i++) { //Recorrer un string
+            System.out.println("Caracter actual: " + cadena.charAt(i));
+        }*/
+    }
+}
