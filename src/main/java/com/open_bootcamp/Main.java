427de2b224c90a8c4386ef5bb294a3efab45dcf4
Jeboy <tovilajesus@gmail.com>
2023-05-22 23:50:43 -0300

    Arrays

--- src/main/java/com/open_bootcamp/Main.java
+++ src/main/java/com/open_bootcamp/Main.java
@@ -3,7 +3,8 @@ package com.open_bootcamp;
 public class Main {
 
     public static void main(String[] args) {
-        /*String cadena = "Mensaje DE TEXTo";
+
+        /* String cadena = "Mensaje DE TEXTo";
 
         int cadenaLen = cadena.length();
         System.out.println("La longitud de mi cadena es de: " + cadenaLen);
@@ -26,6 +27,68 @@ public class Main {
 
         for (int i = 0; i < cadena.length(); i++) { //Recorrer un string
             System.out.println("Caracter actual: " + cadena.charAt(i));
-        }*/
+        }
+
+        int[] arrayUno = {1, 2, 3, 4, 5};
+
+        for (int i : arrayUno) {
+            System.out.println(i);
+        }
+
+        String[] nombres = {
+                "Andrea",
+                "Jesús",
+                "Guadalupe"
+        };
+
+        System.out.println("Longitud del array: " + nombres.length);
+
+        for (String nombre : nombres){ // Forma corta de recorrer un array
+            System.out.println("Nombre actual: " + nombre);
+        }
+
+        for (int i = 0; i < nombres.length; i++) { // Forma larga de recorrer un array (Se obtiene la posición)
+            System.out.println("Nombre actual: " + nombres[i] + " en posición: " + i);
+        }
+
+        int contador = 0; // Recorrer un array con un bucle while
+        while (contador < nombres.length) {
+            System.out.println("Nombre actual: " + nombres[contador] + " en posición: " + contador);
+            contador++;
+        }
+
+        int[][] arrayBidi = new int[2][4];
+        arrayBidi[0][0] = 1;
+        arrayBidi[0][1] = 2;
+        arrayBidi[0][2] = 3;
+        arrayBidi[0][3] = 4;
+
+        arrayBidi[1][0] = 10;
+        arrayBidi[1][1] = 20;
+        arrayBidi[1][2] = 30;
+        arrayBidi[1][3] = 40;
+
+        for (int i = 0; i < arrayBidi.length; i++) { // Recorrer un array Bidi
+            System.out.println("Valor de i: " + i);
+
+            for (int j = 0; j < arrayBidi[1].length; j++) {
+                System.out.println("Estoy en i = " + i + ", j = " + j);
+                System.out.println(arrayBidi[i][j]);
+            }
+        }
+
+        // 2 filas x 4 columnas
+        int[][] arrayBidi1 = {
+                {1, 2, 3, 4},
+                {10, 20, 30, 40}
+        };
+
+        String nombres1[] = {"Andrea", "Jesús"};
+        nombres1[0] = "Guadalupe";
+        nombres1[1] = "Enrique";
+
+        for (String nombre1 : nombres1) {
+            System.out.println(nombre1);
+        } */
     }
 }
