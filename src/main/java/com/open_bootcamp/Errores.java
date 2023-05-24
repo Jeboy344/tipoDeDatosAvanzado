c182fefe8d50d27de25249988f13480ba15cc1c5
Jeboy <tovilajesus@gmail.com>
2023-05-24 15:31:32 -0300

    Tipos de errores

---
+++ src/main/java/com/open_bootcamp/Errores.java
@@ -0,0 +1,35 @@
+package com.open_bootcamp;
+
+import java.util.Scanner;
+
+public class Errores {
+
+    // Tipo de errores
+
+    // 1) Error en tiempo de ejecución: se producen cuando se ejecute el programa y genera que el programa aborte la ejecución
+    public static void errorEjecucion(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce dos números: ");
+
+        int numeroA = scanner.nextInt();
+        int numeroB = scanner.nextInt();
+
+        int resultado = numeroA / numeroB;
+        System.out.println("Resultado: " + resultado);
+    }
+
+    // 2) Error de compilación: ocurren durante el proceso de compilación de un programa de computadora. La compilación es el proceso de convertir el código fuente de un programa escrito
+    // Ej: int numeroB = scanner.nextInt() Dará error de compilación, ya que falta ";"
+
+    // 3) Error lógico: Basicamente son errores de malas instrucciones dadas al programa, errores humanos
+    public static void errorLogico() {
+        int temperatura = 35;
+        if (temperatura < 15) {
+            System.out.println("Hace frío");
+        } else if (temperatura  < 30) {
+            System.out.println("El clima es ideal");
+        }else if (temperatura >= 30){
+            System.out.println("Hace calor");
+        }
+    }
+}
--- src/main/java/com/open_bootcamp/Main.java
+++ src/main/java/com/open_bootcamp/Main.java
@@ -1,8 +1,12 @@
 package com.open_bootcamp;
 
+import static com.open_bootcamp.Errores.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        errorEjecucion();
+        errorLogico();
     }
 }
