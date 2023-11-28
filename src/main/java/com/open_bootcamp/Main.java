7ac201a9957eed06b6aee9049cb6d80a38851ed9
Jeboy <tovilajesus@gmail.com>
2023-11-27 22:52:32 -0300

    Prueba

--- src/main/java/com/open_bootcamp/Main.java
+++ src/main/java/com/open_bootcamp/Main.java
@@ -8,21 +8,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-       /* errorEjecucion();
+        errorEjecucion();
         errorLogico();
         try {
             divide(4, 0);
         }   catch (Exception e) {
         System.out.println("Error");
-        }*/
+        }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce dos números: ");
-
-        int numeroA = scanner.nextInt();
-        int numeroB = scanner.nextInt();
-
-        int resultado = numeroA / numeroB;
-        System.out.println("Resultado es: " + resultado);
     }
 }
