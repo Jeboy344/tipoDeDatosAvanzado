d070c4d9c04cd912ef030ec7ed91b56fd7ef701b
Jeboy <tovilajesus@gmail.com>
2023-11-30 19:00:07 -0300

    Inpustream

--- src/main/java/com/open_bootcamp/Main.java
+++ src/main/java/com/open_bootcamp/Main.java
@@ -1,5 +1,9 @@
 package com.open_bootcamp;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import static com.open_bootcamp.Errores.*;
@@ -8,13 +12,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        errorEjecucion();
+       /* errorEjecucion();
         errorLogico();
         try {
             divide(4, 0);
         }   catch (Exception e) {
         System.out.println("Error");
-        }
+        }*/
+        //ToDo: InputStream
 
+        try {
+            InputStream fichero = new FileInputStream("/etc/passwd");
+
+            try {
+                byte datos []= new byte[5];
+                int dato = fichero.read(datos);
+                while (dato != -1){
+                    // "EOF" (End of File)
+                    System.out.print(datos.toString());
+                    dato = fichero.read(datos);
+                }
+
+            } catch (IOException e) {
+                System.out.println("No puedo leer el fichero: " + e.getMessage());;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("El programa da error: " + e.getMessage());
+        }
     }
 }
